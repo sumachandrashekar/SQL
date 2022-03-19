561639b87588777e3a23006393c790c97c0eaf47
sumachandrashekar <sumachandrashekar2@gmail.com>
2022-03-19 15:10:50 +0530

    Modified datatype to Datetime

--- 8_Weeks_Challenge/SQLSchema/CaseStudy_2_PizzaRunner.sql
+++ 8_Weeks_Challenge/SQLSchema/CaseStudy_2_PizzaRunner.sql
@@ -20,7 +20,7 @@ CREATE TABLE customer_orders (
   "pizza_id" INTEGER,
   "exclusions" VARCHAR(4),
   "extras" VARCHAR(4),
-  "order_time" TIMESTAMP
+  "order_time" DATETIME
 );
 
 INSERT INTO customer_orders
